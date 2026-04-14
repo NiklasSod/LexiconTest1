923632ba2670537afa848e4bc9143d187b9b4931
NiklasSod <nma.prob@gmail.com>
2026-04-14 17:39:10 +0200

    ListGame puzzle added and solved

---
+++ Kattis/ListGame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// https://open.kattis.com/problems/listgame
+
+namespace LexiconTest1.Kattis
+{
+    internal class ListGame
+    {
+        static void Main(string[] args)
+        {
+            //string input = Console.ReadLine();
+            string input = "127381";
+            int player1Number = int.Parse(input);
+            Console.WriteLine(Player2Answer(player1Number));
+            Console.ReadLine();
+        }
+
+        static string Player2Answer(int number)
+        {
+            int restNumber = number;
+            int pointsPlayer2 = 0;
+            int updatedNumber = 2;
+
+            while (updatedNumber * updatedNumber <= restNumber) {
+                for (int i = updatedNumber; i < restNumber; i++)
+                {
+                    while (restNumber % updatedNumber == 0)
+                    {
+                        restNumber = restNumber / updatedNumber;
+                        pointsPlayer2++;
+                    }
+                    updatedNumber++;
+                }
+            }
+
+            if (restNumber > 1)
+            {
+                pointsPlayer2++;
+            }
+
+            return pointsPlayer2.ToString();
+        }
+    }
+}
