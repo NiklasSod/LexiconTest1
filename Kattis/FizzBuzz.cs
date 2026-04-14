6b1cb76e2091c2c327b36ff2031338ef316ba3be
NiklasSod <nma.prob@gmail.com>
2026-04-14 15:27:13 +0200

    fizzbuzz for fun

---
+++ Kattis/FizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LexiconTest1.Kattis
+{
+    internal class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(FizzBuzzSolver(i));
+            }
+            Console.ReadLine();
+        }
+
+        static string FizzBuzzSolver(int number)
+        {
+            if ((number % 3 == 0) && (number % 5 == 0)) return "FizzBuzz";
+            else if (number % 3 == 0) return "Fizz";
+            else if (number % 5 == 0) return "Buzz";
+            else return number.ToString();
+        }
+    }
+}
