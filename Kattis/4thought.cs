a610e1d29e94bc0295ba0e8b86d04583a969e6a5
NiklasSod <nma.prob@gmail.com>
2026-04-15 13:42:10 +0200

    add 4thought problem to work on

---
+++ Kattis/4thought.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LexiconTest1.Kattis
+{
+    internal class _4thought
+    {
+        static void Main(string[] args)
+        {
+            int[] solutions = PreComputeAll();
+            string testLength = Console.ReadLine();
+            if (int.TryParse(testLength, out int testCases)) {
+                for (int i = 0; i < testCases; i++)
+                {
+                    string testCase = Console.ReadLine();
+                    int number = int.Parse(testCase);
+                    // check solutions array and answer in Console.WriteLine
+
+                }
+            }
+        }
+        static int[] PreComputeAll()
+        {
+            // calculate all solutions between the number -60 and 256
+            // sound solution -> save startNum (8) and solution "4 + 4 - 4 + 4 = 8"
+            // return array 
+        }
+    }
+}
